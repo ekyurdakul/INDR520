d01d24894c9864a0ca93e00690c8afa1c839f894
Ekrem Emre Yurdakul <eeyrdkl@gmail.com>
2016-12-07 00:08:41 +0300

    Updated header

--- src/Dijkstra.h
+++ src/Dijkstra.h
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#include <limits>
 #include <list>
 #include <queue>
+#include <math.h>
 using namespace std;
 
 class Dijkstra
 { 
 public:
-    //Time complexity: O(V^2)
     static void OriginalAlgorithm(vector<int> V, vector<vector<float> > E, int source);
-    //Time complexity: O(E + VlogV)
     static void ModifiedAlgorithm(vector<int> V, vector<vector<float> > E, int source);
 };
